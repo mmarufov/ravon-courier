80bf63c3afe1d4837bdea76eb2964d719a837562
Muhammadjon Marufov <mmarufov@dons.usfca.edu>
2026-03-17 15:36:25 -0700

    Track courier app files directly

---
+++ RavonCourier/RavonCourier/RavonCourierApp.swift
@@ -0,0 +1,17 @@
+//
+//  RavonCourierApp.swift
+//  RavonCourier
+//
+//  Created by Muhammad Marufov on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct RavonCourierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
